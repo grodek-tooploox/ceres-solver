02368ab21674b05aa197bfd20e657396ca1d9042
Piotr Grodek <piotr.grodek@tooploox.com>
2017-07-11 10:40:05 +0200

    Comment out sdk version check as it is not working correctly

--- jni/Android.mk
+++ jni/Android.mk
@@ -74,10 +74,11 @@
 
 LOCAL_PATH := $(call my-dir)
 
+# below does not work correctly and it failes even if newer version is used.
 # Ceres requires at least NDK version r9d to compile.
-ifneq ($(shell $(LOCAL_PATH)/assert_ndk_version.sh "r9d" $(NDK_ROOT)), true)
-  $(error Ceres requires NDK version r9d or greater)
-endif
+#ifneq ($(shell $(LOCAL_PATH)/assert_ndk_version.sh "r9d" $(NDK_ROOT)), true)
+#  $(error Ceres requires NDK version r9d or greater)
+#endif
 
 EIGEN_PATH := $(EIGEN_PATH)
 CERES_INCLUDE_PATHS := $(CERES_EXTRA_INCLUDES)
